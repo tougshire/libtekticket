a274b4b5bdba4d0ea6f90abe594cbd3b5ca3352e
tougshire <tougshire@tougshire.com>
2022-01-05 14:49:46 -0500

    fixed error caused by item with no location

--- forms.py
+++ forms.py
@@ -5,7 +5,8 @@ class ItemSelect(forms.Select):
     def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
         option = super().create_option(name, value, label, selected, index, subindex, attrs)
         if value:
-            option['attrs']['data-location'] = value.instance.location.pk
+            if value.instance.location is not None:
+                option['attrs']['data-location'] = value.instance.location.pk
         return option
 
 
