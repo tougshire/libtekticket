a79b849617564b2b1e88265e2fdebab3d5abdb90
tougshire <tougshire@tougshire.com>
2022-01-06 07:51:54 -0500

    fixed error caused by checking perms of non-existing object

--- templatetags/perm_checks.py
+++ templatetags/perm_checks.py
@@ -4,4 +4,8 @@ register = template.Library()
 
 @register.simple_tag
 def user_is_editor(object, user):
-    return object.user_is_editor(user)
+    if object:
+        return object.user_is_editor(user)
+    else:
+        #if there is no object, then the user can create one
+        return True
