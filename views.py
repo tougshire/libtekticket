78ba78d6e778fbebc25f2feae1f0129a5db4b8f3
tougshire <tougshire@tougshire.com>
2022-01-21 06:43:38 -0500

    search includes resolution_notes

--- views.py
+++ views.py
@@ -251,6 +251,9 @@ class TicketList(PermissionRequiredMixin, ListView):
             'item__mmodel__model_name',
             'item__primary_id',
             'submitted_by__display_name',
+            'resolution_notes',
+            'short_description',
+            'long_description',
     ]
 
     for fieldname in ['when', 'item', 'urgency',]:
@@ -323,159 +326,6 @@ class TicketList(PermissionRequiredMixin, ListView):
             context_data['vista__name'] = self.request.POST.get('vista__name')
 
         context_data['ticket_labels'] = { field.name: field.verbose_name.title() for field in Ticket._meta.get_fields() if type(field).__name__[-3:] != 'Rel' }
-
-        return context_data
-
-
-class xTicketList(PermissionRequiredMixin, ListView):
-    permission_required = 'libtekticket.view_ticket'
-    model = Ticket
-    filter_object = {}
-    exclude_object = {}
-    order_by = []
-    order_by_fields = []
-    for fieldname in ['when', 'urgency', 'item',]:
-        order_by_fields.append(
-            {'name': fieldname, 'label': Ticket._meta.get_field(
-                fieldname).verbose_name.title()}
-        )
-        order_by_fields.append(
-            {'name': '-' + fieldname, 'label': '{} reverse'.format(
-                Ticket._meta.get_field(fieldname).verbose_name.title())}
-        )
-
-    def post(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
-
-    def get_queryset(self):
-
-        order_by = []
-        filter_object = {}
-
-        if 'query_submitted' in self.request.POST:
-
-            for i in range(0, 3):
-                order_by_i = 'order_by_{}'.format(i)
-                if order_by_i in self.request.POST:
-                    for field in self.order_by_fields:
-                        if self.request.POST.get(order_by_i) == field['name']:
-                            order_by.append(field['name'])
-
-            for fieldname in ['mmodel', 'urgency', 'role']:
-                filterfieldname = 'filter__' + fieldname + '__in'
-                if filterfieldname in self.request.POST and self.request.POST.get(filterfieldname) > '':
-                    postfields = self.request.POST.getlist(filterfieldname)
-                    fieldlist = []
-                    for postfield in postfields:
-                        if postfield.isdecimal():
-                            fieldlist.append(postfield)
-                    if fieldlist:
-                        filter_object[fieldname + '__in'] = postfields
-
-                filterfieldnone = 'filter__' + fieldname + '__none'
-                if filterfieldnone in self.request.POST:
-                    filter_object[fieldname] = None
-
-            vista__name = ''
-            if 'vista__name' in self.request.POST and self.request.POST.get('vista__name') > '':
-                vista__name = self.request.POST.get('vista__name')
-
-            if filter_object or order_by:
-
-                queryset = super().get_queryset()
-
-                vista, created = Vista.objects.get_or_create(
-                    user=self.request.user, model_name='libtekticket.ticket', name=vista__name)
-
-                if filter_object:
-                    self.filter_object = filter_object
-                    vista.filterstring = json.dumps(filter_object)
-                    queryset = queryset.filter(**filter_object)
-
-                if order_by:
-                    self.order_by = order_by
-                    vista.sortstring = ','.join(order_by)
-                    queryset = queryset.order_by(*order_by)
-
-                vista.save()
-
-                return queryset
-
-        elif 'get_vista' in self.request.POST:
-
-            vista__name = ''
-
-            if 'vista__name' in self.request.POST and self.request.POST.get('vista__name') > '':
-                vista__name = self.request.POST.get('vista__name')
-
-            vista, created = Vista.objects.get_or_create(
-                user=self.request.user, model_name='libtekticket.ticket', name=vista__name)
-
-            try:
-                filter_object = json.loads(vista.filterstring)
-                order_by = vista.sortstring.split(',')
-                queryset = super().get_queryset().filter(**filter_object).order_by(*order_by)
-                self.filter_object = filter_object
-
-                return queryset
-
-            except json.JSONDecodeError:
-                pass
-
-        elif 'delete_vista' in self.request.POST:
-
-            vista__name = ''
-
-            if 'vista__name' in self.request.POST and self.request.POST.get('vista__name') > '':
-
-                vista__name = self.request.POST.get('vista__name')
-                Vista.objects.filter(
-                    user=self.request.user, model_name='libtekticket.ticket', name=vista__name).delete()
-
-        # this code runs if no queryset has been returned yet
-            vista = Vista.objects.filter(
-                user=self.request.user, is_default=True).last()
-            if vista is None:
-                vista = Vista.objects.filter(user=self.request.user).last()
-            if vista is None:
-                vista, created = Vista.objects.get_or_create(
-                    user=self.request.user)
-
-            try:
-                filter_object = json.loads(vista.filterstring)
-                order_by = vista.sortstring.split(',')
-                try:
-                    self.filter_object = filter_object
-                    self.order_by = order_by
-
-                    return super().get_queryset().filter(**filter_object).order_by(order_by)
-
-                except (ValueError, TypeError, FieldError):
-                    print('Field/Value/Type Error')
-                    vista.delete()
-
-            except json.JSONDecodeError:
-                print('deserialization error')
-                vista.delete()
-
-        return super().get_queryset()
-
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['items'] = Item.objects.all()
-        context_data['mmodels'] = Mmodel.objects.all()
-        context_data['users'] = get_user_model().objects.all()
-        context_data['vistas'] = Vista.objects.filter(
-            user=self.request.user, model_name='libtekticket.ticket').all()
-        context_data['ordering'] = Ticket._meta.ordering
-        context_data['order_by_fields'] = self.order_by_fields
-        if self.filter_object:
-            context_data['filter_object'] = self.filter_object
-        if self.request.POST.get('vista__name'):
-            context_data['vista__name'] = self.request.POST.get('vista__name')
-
-        context_data['ticket_labels'] = {field.name: field.verbose_name.title(
-        ) for field in Ticket._meta.get_fields() if type(field).__name__[-3:] != 'Rel'}
 
         return context_data
 
