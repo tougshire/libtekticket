631267ca9b5557d03d4c087513f1f2d8e5519202
tougshire <tougshire@tougshire.com>
2022-02-01 14:55:45 -0500

    changes to accomodate changes in tougshire_vistas

--- views.py
+++ views.py
@@ -15,9 +15,9 @@ from tougshire_vistas.models import Vista
 from django.contrib.auth import get_user_model
 from .forms import TicketForm, TicketTicketNoteForm, TicketTicketNoteFormset
 from .models import Technician, History, Ticket, TicketNote
-from tougshire_vistas.views import get_vista_object
+from tougshire_vistas.views import make_vista, retrieve_vista
 
-from libtekin.models import Item, Mmodel
+from libtekin.models import Item, Mmodel, Location
 
 
 def update_history(form, modelname, object, user):
@@ -233,91 +233,111 @@ class TicketSoftDelete(PermissionRequiredMixin, UpdateView):
 class TicketList(PermissionRequiredMixin, ListView):
     permission_required = 'libtekticket.view_ticket'
     model = Ticket
-    filter_object = {}
-    exclude_object = {}
-    order_by = []
-    order_by_fields=[]
-    combined_text_search=""
-    combined_text_fields=[
-            'item__common_name',
-            'item__mmodel__model_name',
-            'item__primary_id',
-            'submitted_by__display_name',
-            'resolution_notes',
+
+    def setup(self, request, *args, **kwargs):
+
+        self.vista_settings={
+            'text_fields_available':[],
+            'filter_fields_available':{},
+            'order_by_fields_available':[],
+            'columns_available':[]
+        }
+
+        self.vista_settings['text_fields_available']=[
             'short_description',
             'long_description',
-    ]
+            'resolution_notes',
+            'item__common_name',
+            'item__mmodel__brand',
+            'submitted_by__display_name',
+        ]
 
-    for fieldname in ['when', 'item', 'urgency']:
-        order_by_fields.append(
-            { 'name':fieldname, 'label':Ticket._meta.get_field(fieldname).verbose_name.title() }
-        )
-        order_by_fields.append(
-            { 'name':'-' + fieldname, 'label':'{} reverse'.format(Ticket._meta.get_field(fieldname).verbose_name.title()) }
-        )
-    filter_fields = {
-        'in':['item', 'item__mmodel', 'urgency','submitted_by'],
-        'exact':['is_resolved'],
-        'after':['when']
-    }
-    # showable_columns = []
-    # show_columns = []
-    # for fieldname in [
-    #         'common_name',
-    #         'mmodel',
-    #         'primary_id_field',
-    #         'serial_number',
-    #         'service_number',
-    #         'asset_number',
-    #         'barcode',
-    #         'condition',
-    #         'network_name',
-    #         'assignee',
-    #         'owner',
-    #         'borrower',
-    #         'home',
-    #         'location',
-    #         'role',
-    #         'latest_inventory'
-    #     ]:
-    #     showable_columns.append(
-    #         {
-    #             'name':fieldname,
-    #             'label':Ticket._meta.get_field(fieldname).verbose_name.title()
-    #         }
-    #     )
-    #     show_columns.append(fieldname)
+        self.vista_settings['filter_fields_available'] = [
+            'item',
+            'location',
+            'urgency',
+            'submitted_by',
+            'is_resolved',
+            'when'
+        ]
+
+        for fieldname in ['is_resolved', 'urgency', 'when', 'submitted_by', 'item',]:
+            self.vista_settings['order_by_fields_available'].append(
+                { 'name':fieldname, 'label':Ticket._meta.get_field(fieldname).verbose_name.title() }
+            )
+            self.vista_settings['order_by_fields_available'].append(
+                { 'name':'-' + fieldname, 'label':'{} reverse'.format(Ticket._meta.get_field(fieldname).verbose_name.title()) }
+            )
+
+        for fieldname in [
+            'is_resolved',
+            'urgency',
+            'when',
+            'submitted_by',
+            'item',
+            'short_description',
+        ]:
+            self.vista_settings['columns_available'].append(
+                {
+                    'name':fieldname,
+                    'label':Ticket._meta.get_field(fieldname).verbose_name.title()
+                }
+            )
+
+        return super().setup(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
 
-        vista_object = get_vista_object(self, super().get_queryset(), 'libtekticket.ticket' )
-        self.filter_object = vista_object['filter_object']
-        self.order_by = vista_object['order_by']
-        self.show_columns = vista_object['show_columns']
-        self.combined_text_search = vista_object['combined_text_search']
-        return vista_object['queryset']
+        self.vista_context = {
+            'show_columns':[],
+            'order_by':[],
+            'combined_text_search':'',
+        }
+
+        queryset = super().get_queryset()
+        vistaobj={'context':{}, 'queryset':queryset}
+        if 'vista_query_submitted' in self.request.POST:
+            vistaobj = make_vista(self.request, self.vista_settings, super().get_queryset())
+        elif 'get_vista' in self.request.POST:
+            vistaobj = retrieve_vista(self.request, self.vista_settings, super().get_queryset())
+
+        for key in vistaobj['context']:
+            self.vista_context[key] = vistaobj['context'][key]
+
+
+        queryset = vistaobj['queryset']
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
+        context_data['items'] = Item.objects.filter(ticket__gt=0)
         context_data['mmodels'] = Mmodel.objects.all()
-        context_data['urgencies'] = Ticket.URGENCY_CHOICES
-        context_data['vistas'] = Vista.objects.filter(user=self.request.user, model_name='libtekticket.ticket').all()
-        context_data['order_by_fields'] = self.order_by_fields
-        context_data['order_by'] = self.order_by
-        # context_data['showable_columns'] = self.showable_columns
-        # context_data['show_columns'] = self.show_columns
-        context_data['combined_text_search'] = self.combined_text_search
-        context_data['ordering'] = Ticket._meta.ordering
-        context_data['items'] = Item.objects.all()
         context_data['users'] = get_user_model().objects.all()
+        context_data['locations'] = Location.objects.all()
+        context_data['vistas'] = Vista.objects.filter(user=self.request.user, model_name='libtekticket.ticket').all()
+        context_data['order_by_fields_available'] = self.vista_settings['order_by_fields_available']
+        context_data['columns_available'] = self.vista_settings['columns_available']
+        context_data['ordering'] = Ticket._meta.ordering
 
-        if self.filter_object:
-            context_data['filter_object'] = self.filter_object
         if self.request.POST.get('vista__name'):
             context_data['vista__name'] = self.request.POST.get('vista__name')
+
+        if self.vista_context:
+            if 'filter' in self.vista_context:
+                for key in self.vista_context['filter']:
+                    context_data[key] = self.vista_context['filter'][key]
+
+        for key in [
+            'combined_text_search',
+            'text_fields_chosen',
+            'order_by'
+            ]:
+            if key in self.vista_context:
+                context_data[key] = self.vista_context[key]
 
         context_data['ticket_labels'] = { field.name: field.verbose_name.title() for field in Ticket._meta.get_fields() if type(field).__name__[-3:] != 'Rel' }
 
