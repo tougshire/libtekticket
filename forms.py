e3d8863d1a748feca92e7ac2aa5a5512a5366b8c
tougshire <tougshire@tougshire.com>
2024-04-30 07:44:34 -0400

    Change attrs for itrem select

--- forms.py
+++ forms.py
@@ -2,18 +2,29 @@ from django import forms
 from django.forms import inlineformset_factory
 from .models import Ticket, TicketNote
 
+
 class ItemSelect(forms.Select):
-    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
-        option = super().create_option(name, value, label, selected, index, subindex, attrs)
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        option = super().create_option(
+            name, value, label, selected, index, subindex, attrs
+        )
         if value:
-            textforfilter = f'{name}|{value.instance.primary_id}'
+            textforfilter = f"{name}|{value.instance.primary_id}"
             if value.instance.location is not None:
-                option['attrs']['data-home'] = value.instance.home.pk
-                textforfilter = textforfilter + f'|{value.instance.home.short_name}|{value.instance.home.full_name}'
+                option["attrs"]["data-home"] = value.instance.home.pk
+                textforfilter = (
+                    textforfilter
+                    + f"|{value.instance.home.short_name}|{value.instance.home.full_name}"
+                )
 
             if value.instance.assignee is not None:
-                textforfilter = textforfilter + f'|{value.instance.assignee.friendly_name}|{value.instance.assignee.full_name}'
-                option['attrs']['data-textforfilter'] = textforfilter
+                textforfilter = (
+                    textforfilter
+                    + f"|{value.instance.assignee.friendly_name}|{value.instance.assignee.full_name}"
+                )
+                option["attrs"]["data-textforfilter"] = textforfilter
 
         return option
 
@@ -25,20 +36,20 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = [
-            'item',
-            'location',
-            'short_description',
-            'long_description',
-            'urgency',
-            'technician',
-            'is_resolved',
-            'recipient_emails',
-            'resolution_notes',
+            "item",
+            "location",
+            "short_description",
+            "long_description",
+            "urgency",
+            "technician",
+            "is_resolved",
+            "recipient_emails",
+            "resolution_notes",
         ]
         widgets = {
-            'item':ItemSelect,
-            'short_description':forms.TextInput(attrs={'class':'widthlong'}),
-            'long_description':forms.Textarea(attrs={'class':'widthlong'}),
+            "item": ItemSelect,
+            "short_description": forms.TextInput(attrs={"class": "widthlong"}),
+            "long_description": forms.Textarea(attrs={"class": "widthlong"}),
         }
 
 
@@ -46,13 +57,26 @@ class TicketTicketNoteForm(forms.ModelForm):
     class Meta:
         model = TicketNote
         fields = [
-            'when',
-            'maintext',
+            "when",
+            "maintext",
         ]
-        widgets={
-            'when':forms.DateInput( attrs={ "type":"date" } ),
-            'maintext':forms.TextInput( attrs={ "class":"widthlong"}),
-            'details':forms.Textarea( attrs={ "class":"widthlong"})
+        widgets = {
+            "when": forms.DateInput(attrs={"type": "date"}),
+            "maintext": forms.TextInput(attrs={"class": "widthlong"}),
+            "details": forms.Textarea(attrs={"class": "widthlong"}),
         }
 
-TicketTicketNoteFormset = inlineformset_factory(Ticket, TicketNote, form=TicketTicketNoteForm, extra=10)
+
+class CSVOptionForm(forms.Form):
+
+    make_csv = forms.BooleanField(
+        label="CSV",
+        initial=False,
+        required=False,
+        help_text="Download the result as a CSV file",
+    )
+
+
+TicketTicketNoteFormset = inlineformset_factory(
+    Ticket, TicketNote, form=TicketTicketNoteForm, extra=10
+)
